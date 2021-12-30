8bb96f2e1e68fd4ec91c9184ffb96152b01ff308
Fatoumata SOUKOUNA <soukounafatoumata86@gmail.com>
2021-12-30 19:26:25 +0100

    uploading on heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Character from './components/Character'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      characters: []
+    }
+  }
+
+  componentDidMount = () => {
+    fetch("https://thronesapi.com/api/v2/Characters")
+      .then(result => result.json())
+      .then(result => this.setState({ characters: result }))
+  }
+
+	render() {
+    console.log(this.state)
+		return(
+      <>
+			<h1 className="text-center">Game of thrones</h1>
+      <ul className="d-flex justify-content-evenly flex-wrap">
+      {this.state.characters.map((item) => {
+            console.log(item)
+            return (
+              <Character name={item.fullName} title={item.title} image={item.imageUrl}/>
+            )
+          })}
+          </ul>
+      </>
+		)
+	}
 }
 
-export default App;
+export default App
